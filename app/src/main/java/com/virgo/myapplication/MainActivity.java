cb1f799da46f19a0f448f0655f9b77eb26eabf6f
gaoguanqi <307590625@qq.com>
2017-03-23 09:28:09 +0800

    增加自定义验证码按钮

--- app/src/main/java/com/virgo/myapplication/MainActivity.java
+++ app/src/main/java/com/virgo/myapplication/MainActivity.java
@@ -2,13 +2,17 @@ package com.virgo.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.virgo.myapplication.dialog.LoadingDialog;
+import com.virgo.myapplication.widget.VerifyCodeButton;
 
 public class MainActivity extends AppCompatActivity  {
-
+private VerifyCodeButton verifyCodeButton;
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +21,22 @@ public class MainActivity extends AppCompatActivity  {
             @Override
             public void onClick(View view) {
                 doButton();
+            }
+        });
+        verifyCodeButton = (VerifyCodeButton) findViewById(R.id.verifyCodeButton);
+        editText = (EditText) findViewById(R.id.editText);
+        verifyCodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 校验手机号是否输入正确
+                String phone = editText.getText().toString();
+                if(!TextUtils.isEmpty(phone)){
+                    Toast.makeText(MainActivity.this, "执行发送验证码操作", Toast.LENGTH_SHORT).show();
+                    // 开始倒计时
+                    verifyCodeButton.start();
+                }else{
+                    Toast.makeText(MainActivity.this, "请输入手机号", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
